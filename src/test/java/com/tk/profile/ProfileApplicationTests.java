af27283eab557282b0c0d474c2900a9ebde5ef44
adrasa.cantya <adrasa.cantya@ui.ac.id>
2024-11-07 15:26:57 +0700

    feat: initializing profile

---
+++ src/test/java/com/tk/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tk.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
