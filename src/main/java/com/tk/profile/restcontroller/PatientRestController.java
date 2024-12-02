612495598009645dbfe5a64e5aeb7348fe5f5efb
adrasa.cantya <adrasa.cantya@ui.ac.id>
2024-12-02 13:50:05 +0700

    feat: add cors config and get patient by id

--- src/main/java/com/tk/profile/restcontroller/PatientRestController.java
+++ src/main/java/com/tk/profile/restcontroller/PatientRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/patient")
@@ -29,7 +30,7 @@ public class PatientRestController {
         return new ResponseEntity<>(baseResponseDTO, HttpStatus.OK);
     }
 
-    @GetMapping("/{nik}")
+    @GetMapping("/get-nik/{nik}")
     public ResponseEntity<?> detailPatientByNik(@PathVariable("nik") String nik) {
         var baseResponseDTO = new BaseResponseDTO<PatientResponseDTO>();
 
@@ -50,5 +51,24 @@ public class PatientRestController {
         return new ResponseEntity<>(baseResponseDTO, HttpStatus.OK);
     }
 
+    @GetMapping("/get-id/{id}")
+    public ResponseEntity<?> detailPatientById(@PathVariable("id") UUID id) {
+        var baseResponseDTO = new BaseResponseDTO<PatientResponseDTO>();
 
+        PatientResponseDTO patient = patientRestService.getPatientById(id);
+
+        if (patient == null) {
+            baseResponseDTO.setStatus(HttpStatus.NOT_FOUND.value());
+            baseResponseDTO.setMessage("Data patient tidak ditemukan");
+            baseResponseDTO.setTimestamp(new Date());
+            return new ResponseEntity<>(baseResponseDTO, HttpStatus.NOT_FOUND);
+        }
+
+        baseResponseDTO.setStatus(HttpStatus.OK.value());
+        baseResponseDTO.setData(patient);
+        baseResponseDTO.setMessage(String.format("Patient dengan ID %s berhasil ditemukan", patient.getNik()));
+        baseResponseDTO.setTimestamp(new Date());
+
+        return new ResponseEntity<>(baseResponseDTO, HttpStatus.OK);
+    }
 }
